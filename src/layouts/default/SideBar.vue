b19e67d9ea38cc8e86d5e2297a49514d02e317bd
Gülsüm Vural <gulsum-vural@yandex.com>
2023-12-30 13:26:21 +0300

    fix: :art: change code

--- src/layouts/default/SideBar.vue
+++ src/layouts/default/SideBar.vue
@@ -1,70 +1,30 @@
 <template>
     <v-layout>
+      <!-- burası side bar kısmı sayfa akışında fix kalıcak -->
       <v-navigation-drawer
         permanent
         theme="dark"
       >
+      <div class="logo">
+        <v-img
+          :width="300"
+          cover
+          src="../../assets/logo.svg"
+        ></v-img>
+      </div>
       <v-list nav>
-          <v-list-item class="logo">
-            <v-img
-              :width="300"
-              cover
-              src="../../assets/logo.svg"
-            ></v-img>
-          </v-list-item>
-          <v-list-item
-            style="margin-top: 30px;"
-            prepend-icon="mdi-home-outline"
-            :title="$t('menu.home')"
-            value="home"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-pound"
-            :title="$t('menu.trends')"
-            value="trends"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-bell-outline"
-            :title="$t('menu.notifications')"
-            value="notifications"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-inbox-outline"
-            :title="$t('menu.message')"
-            value="message"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-bookmark-outline"
-            :title="$t('menu.bookmarks')"
-            value="bookmarks"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-list-box-outline"
-            :title="$t('menu.list')"
-            value="lists"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-account-outline"
-            :title="$t('menu.profile')"
-            value="profile"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-dots-horizontal-circle-outline"
-            :title="$t('menu.more')"
-            value="more"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-cart-minus"
-            :title="$t('menu.shop')"
-            value="shop"
-            class="shop-menu-btn"
-            style="color: #fff"
-          ></v-list-item>
-          <v-list-item
-            style="bottom: 10px;position: fixed;"
+        <v-list-item v-for="(item,index) in value" :key="index" :value="item.text" :title="$t(`menu.${item.text}`)" @click="clickMenuItem(item.text)">
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+          <v-list-item-title :v-text="item.text"></v-list-item-title>
+        </v-list-item>
+        </v-list>
+        <div
+            style="bottom: 20px;position: fixed;left: 50px; cursor: pointer"
           >
             <div class="profile">
-              <div style="display: flex;">
+              <div style="display: flex;" @click="goToProfile()">
                 <v-img
                   :width="40"
                   :height="40"
@@ -72,18 +32,18 @@
                   src="../../assets/profile.svg"
                   class="profile-img"
                   ></v-img>
-                  <div style="display: grid;margin-left: 10px;">
+                  <div style="display: grid;margin-left: 10px;font-size: 16px;">
                     <span>{{ user.userName }}</span>
-                    <span>@{{ user.userId }}</span>
+                    <span style="font-style: normal;">@{{ user.userId }}</span>
                   </div>
               </div>
               <div style="padding-left: 60px;padding-top: 10px;">
                 <v-icon icon="mdi-dots-horizontal"></v-icon>
               </div>
             </div>
-          </v-list-item>
-        </v-list>
+          </div>
       </v-navigation-drawer>
+      <!-- burası view kısmı, menuye göre değişcek tek değişcek alan bu kısım -->
       <v-main>
         <default-view />
       </v-main>
@@ -96,10 +56,29 @@ import i18n from '@/i18n';
 
 const { t } = i18n;
 
+const value = [
+  {text: 'home', icon: 'mdi-home-outline'},
+  {text: 'trends', icon: 'mdi-pound'},
+  {text: 'notifications', icon: 'mdi-bell-outline'},
+  {text: 'message', icon: 'mdi-inbox-outline'},
+  {text: 'bookmarks', icon: 'mdi-bookmark-outline'},
+  {text: 'lists', icon: 'mdi-list-box-outline'},
+  {text: 'more', icon: 'mdi-dots-horizontal-circle-outline'},
+  {text: 'shop', icon: 'mdi-cart-minus'}
+];
+
 const user = {
   userName: 'Gülsüm Vural',
   userId: 'glsmvrl'
 };
+
+function clickMenuItem(value) {
+  console.log('clicked item',value);
+}
+
+function goToProfile() {
+  console.log('clicked profile');
+}
 
 </script>
 
@@ -108,10 +87,13 @@ const user = {
   background-color: #fff;
   width: 100%;
   height: 100vh;
+  font-size: 20px;
+  font-style: italic;
 }
 
 .logo {
   width: 100%;
+  margin-top: 30px;
 
   .v-img {
     width: 100% !important;
@@ -121,6 +103,7 @@ const user = {
 .v-navigation-drawer {
   width: 340px !important;
   background-color: #FBF5E6;
+  position: fixed;
 
   .v-navigation-drawer--left  {
     border-right-width: none;
@@ -131,26 +114,26 @@ const user = {
       margin: 20px 30px;
       .v-list-item {
         color: #000;
+        cursor: pointer;
+      }
+
+      .v-list-item:hover {
+        color: #fa9392;
+      }
+      .v-list-item:focus {
+        color: #fa9392;
       }
     }
   }
+}
 
-  .shop-menu-btn {
-    margin-top: 30px;
-    width: 200px;
-    height: 40px;
-    background-color: #fa9392;
-    border-radius: 100px;
-    padding: 0px 30px;
-  }
-
-  .profile {
+.profile {
     display: flex;
     justify-content: space-between;
+    color: #000;
     .profile-img {
       border-radius: 100%;
       align-items: center;
     }
   }
-}
 </style>
