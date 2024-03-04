23e871cd6864dbad461f9b644a83d969db45089a
Gülsüm Vural <gulsum-vural@yandex.com>
2024-03-04 18:26:38 +0300

    fix: router for page

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        component: () => import('@/views/Home.vue'),
-      },
-    ],
+    path: '',
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,31 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <HomeView />
+  <v-app>
+    <v-row no-gutters>
+      <v-col cols="12" sm="3" style="max-width: 23%">
+        <left-bar />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <HomeView />
+      </v-col>
+      <v-col cols="12" sm="3">
+        <v-divider vertical></v-divider>
+        <right-bar style="padding: 20px" />
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script setup>
+import LeftBar from '@/layouts/default/LeftBar.vue';
+import RightBar from '@/layouts/default/RightBar.vue';
 import HomeView from '@/components/Home/HomeView.vue';
 </script>
+
+<style lang="scss" scoped>
+.v-divider {
+  height: 100vh !important;
+  z-index: 10000;
+  position: fixed !important;
+}
+</style>
